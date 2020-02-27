1d3766f83b00eeca62fc38b89b912b41795066be
suzanne werner <capnsue@gmail.com>
2020-02-27 13:31:29 -0500

    latest external table scripts for cbioportal

---
+++ java/MySQLTranslator/cbioportal202002271308-drop.sql
@@ -0,0 +1,116 @@
+DROP EXTERNAL TABLE [type_of_cancer] ;
+
+DROP EXTERNAL TABLE [reference_genome] ;
+
+DROP EXTERNAL TABLE [cancer_study] ;
+
+DROP EXTERNAL TABLE [cancer_study_tags] ;
+
+DROP EXTERNAL TABLE [users] ;
+
+DROP EXTERNAL TABLE [authorities] ;
+
+DROP EXTERNAL TABLE [patient] ;
+
+DROP EXTERNAL TABLE [sample] ;
+
+DROP EXTERNAL TABLE [sample_list] ;
+
+DROP EXTERNAL TABLE [sample_list_list] ;
+
+DROP EXTERNAL TABLE [genetic_entity] ;
+
+DROP EXTERNAL TABLE [gene] ;
+
+DROP EXTERNAL TABLE [gene_alias] ;
+
+DROP EXTERNAL TABLE [geneset] ;
+
+DROP EXTERNAL TABLE [geneset_gene] ;
+
+DROP EXTERNAL TABLE [geneset_hierarchy_node] ;
+
+DROP EXTERNAL TABLE [geneset_hierarchy_leaf] ;
+
+DROP EXTERNAL TABLE [treatment] ;
+
+DROP EXTERNAL TABLE [uniprot_id_mapping] ;
+
+DROP EXTERNAL TABLE [genetic_profile] ;
+
+DROP EXTERNAL TABLE [genetic_profile_link] ;
+
+DROP EXTERNAL TABLE [genetic_alteration] ;
+
+DROP EXTERNAL TABLE [genetic_profile_samples] ;
+
+DROP EXTERNAL TABLE [gene_panel] ;
+
+DROP EXTERNAL TABLE [gene_panel_list] ;
+
+DROP EXTERNAL TABLE [sample_profile] ;
+
+DROP EXTERNAL TABLE [structural_variant] ;
+
+DROP EXTERNAL TABLE [mutation_event] ;
+
+DROP EXTERNAL TABLE [mutation] ;
+
+DROP EXTERNAL TABLE [mutation_count_by_keyword] ;
+
+DROP EXTERNAL TABLE [clinical_patient] ;
+
+DROP EXTERNAL TABLE [clinical_sample] ;
+
+DROP EXTERNAL TABLE [clinical_attribute_meta] ;
+
+DROP EXTERNAL TABLE [interaction] ;
+
+DROP EXTERNAL TABLE [mut_sig] ;
+
+DROP EXTERNAL TABLE [protein_array_info] ;
+
+DROP EXTERNAL TABLE [protein_array_target] ;
+
+DROP EXTERNAL TABLE [protein_array_data] ;
+
+DROP EXTERNAL TABLE [protein_array_cancer_study] ;
+
+DROP EXTERNAL TABLE [sanger_cancer_census] ;
+
+DROP EXTERNAL TABLE [gistic] ;
+
+DROP EXTERNAL TABLE [gistic_to_gene] ;
+
+DROP EXTERNAL TABLE [text_cache] ;
+
+DROP EXTERNAL TABLE [pfam_graphics] ;
+
+DROP EXTERNAL TABLE [drug] ;
+
+DROP EXTERNAL TABLE [drug_interaction] ;
+
+DROP EXTERNAL TABLE [cna_event] ;
+
+DROP EXTERNAL TABLE [sample_cna_event] ;
+
+DROP EXTERNAL TABLE [copy_number_seg] ;
+
+DROP EXTERNAL TABLE [copy_number_seg_file] ;
+
+DROP EXTERNAL TABLE [cosmic_mutation] ;
+
+DROP EXTERNAL TABLE [pdb_uniprot_alignment] ;
+
+DROP EXTERNAL TABLE [pdb_uniprot_residue_mapping] ;
+
+DROP EXTERNAL TABLE [clinical_event] ;
+
+DROP EXTERNAL TABLE [clinical_event_data] ;
+
+DROP EXTERNAL TABLE [reference_genome_gene] ;
+
+DROP EXTERNAL TABLE [data_access_tokens] ;
+
+DROP EXTERNAL TABLE [info] ;
+
